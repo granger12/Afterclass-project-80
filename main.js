1ed18285167bc92d633ba5db57928b23949ad357
granger12 <73669890+granger12@users.noreply.github.com>
2021-02-17 14:09:11 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-function getParagraph1(){
-    var inputs=[];
-    for(var i=1; i <=6; i++){
-        inputs.push(document.getElementById("paral_input_box"+ i).value)
+
+function getParagraph1()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("paral_input_box_" + i).value);
     }
-    inputs.join(". ")
-    document.getElementById("showParagraph1").innerHTML = inputs.join(". ")
+document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
 }
 
-
-
-function getParagraph2(){
-    var inputs=[];
-    for(var i=1; i <=6; i++){
-        inputs.push(document.getElementById("paral_input_box"+ i).value)
+function getParagraph2()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("paral_input_box_" + i).value);
     }
-    inputs.join(". ")
-    document.getElementById("showParagraph2").innerHTML = inputs.join(". ")
+document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
 }
